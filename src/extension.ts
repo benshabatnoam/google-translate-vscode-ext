532b268b0c45de05be53d548fb13569461bd1ef1
Noam Ben Shabat <noamb@Taldor.corp>
2018-02-15 12:39:59 +0200

    Use google-translate api

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,11 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+var googleTranslate = require('google-translate')('AIzaSyBJKuSQ6xyOq4Bjda3rsdf0n--7LU2O4I4');
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	let disposable = vscode.commands.registerCommand('extension.translate', () => {
 		let editor = vscode.window.activeTextEditor;
 		if (!editor){
@@ -15,9 +16,16 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		let selection = editor.selection;
 		let selectedText = editor.document.getText(new vscode.Range(selection.start, selection.end));
-		if (!selectedText)		
+		if (!selectedText) {
 			vscode.window.showErrorMessage('Must select text to translate');
-		vscode.window.showInformationMessage(selectedText);
+			return;
+		}
+		googleTranslate.translate(selectedText, 'he', (err: any, translation: any) => {
+			if (err)
+				vscode.window.showErrorMessage(err.body);
+			else if (translation)
+				vscode.window.showInformationMessage(translation.translatedText);
+		});
 	});
 
 	context.subscriptions.push(disposable);
