5b22a538b0537eda29eba729ac5d65c0a63febe5
Noam Ben Shabat <noamb@Taldor.corp>
2018-05-16 18:52:20 +0300

    multi line translation is now working with replace feature

--- src/extension.ts
+++ src/extension.ts
@@ -2,85 +2,97 @@
 
 import * as vscode from 'vscode';
 
+var googleTranslate: any;
+var languages: any;
 var replaceText: boolean;
-var translations: any[] = [];
+var translations: any[];
 var selections: vscode.Selection[];
+var linesCount: number;
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.translate', () => {
-		if (!vscode.window.activeTextEditor) {
-			vscode.window.showErrorMessage('Must select text to translate');
-			return;
-		}
-		replaceText = vscode.workspace.getConfiguration('googleTranslateExt')['replaceText'];
-		selections = vscode.window.activeTextEditor.selections;
-		if (selections.length > 1) {
-			if (selections.every(s => s.isEmpty)) {
-				showEmptyError();
-				return;
-			}
-			multiCursorTranslate();
-		}
-		else if (selections.length === 1) {
-			let selection: vscode.Selection = selections[0];
-			if (selection.isEmpty) {
-				showEmptyError();
-				return;
-			}
-			translate(selections[0]);
-		}
-		else {
-			// show error from above
-		}
-	});
+	let disposable = vscode.commands.registerCommand('extension.translate', onActivate);
 	context.subscriptions.push(disposable);
 }
 
-function multiCursorTranslate() {
+function onActivate(): void {
+	if (!vscode.window.activeTextEditor) {
+		vscode.window.showErrorMessage('Must select text to translate');
+		return;
+	}
+	initMembers();
+	if (selections.length > 1) {
+		if (selections.every(s => s.isEmpty)) {
+			showEmptyError();
+			return;
+		}
+		multiCursorTranslate();
+	}
+	else if (selections.length === 1) {
+		let selection: vscode.Selection = selections[0];
+		if (selection.isEmpty) {
+			showEmptyError();
+			return;
+		}
+		translateSelection(selections[0]);
+	}
+	else {
+		showEmptyError();
+	}
+}
+
+function initMembers(): void {
+	let apiKey = vscode.workspace.getConfiguration('googleTranslateExt')['apiKey'];
+	googleTranslate = require('google-translate')(apiKey);
+	languages = vscode.workspace.getConfiguration('googleTranslateExt')['languages'];
+	replaceText = vscode.workspace.getConfiguration('googleTranslateExt')['replaceText'];
+	selections = vscode.window.activeTextEditor.selections;
+	translations = [];
+	linesCount = 0;
+}
+
+function multiCursorTranslate(): void {
 	selections.forEach(selection => {
-		translate(selection);
+		translateSelection(selection);
 	});
 }
 
-function translate(selection: vscode.Selection | vscode.Range) {
+function translateSelection(selection: vscode.Selection | vscode.Range): void {
 	if (!selection.isSingleLine) {
-		let firstLine: number = selection.start.line;
-		let lastLine: number = selection.end.line;
-		for (let line = firstLine; line <= lastLine; line++) {
-			let range: vscode.Range = vscode.window.activeTextEditor.document.lineAt(line).range;
-			if (line === firstLine) {
-				range = new vscode.Range(line, selection.start.character, line, range.end.character);
-			} else if (line === lastLine) {
-				range = new vscode.Range(line, 0, line, selection.end.character);
+		let firstLineNumber: number = selection.start.line;
+		let lastLineNumber = selection.end.line;
+		linesCount += lastLineNumber - firstLineNumber;
+		for (let lineNumber = firstLineNumber; lineNumber <= lastLineNumber; lineNumber++) {
+			let range: vscode.Range = vscode.window.activeTextEditor.document.lineAt(lineNumber).range;
+			if (lineNumber === firstLineNumber) {
+				range = new vscode.Range(lineNumber, selection.start.character, lineNumber, range.end.character);
+			} else if (lineNumber === lastLineNumber) {
+				range = new vscode.Range(lineNumber, 0, lineNumber, selection.end.character);
 			}
-			translate(range);
+			translateSelection(range);
 		}
 		return;
 	}
 	let selectedText: string = vscode.window.activeTextEditor.document.getText(new vscode.Range(selection.start, selection.end));
-	let apiKey = vscode.workspace.getConfiguration('googleTranslateExt')['apiKey'];
-	let googleTranslate = require('google-translate')(apiKey);
-	let languages: any = vscode.workspace.getConfiguration('googleTranslateExt')['languages'];
 	if (!languages) {
 		vscode.window.showErrorMessage('Go to user settings and edit "googleTranslateExt.languages".');
 		return;
 	}
 	if (typeof languages === "string") {
-		googleTranslate.translate(selectedText, languages, onTranslated);
+		googleTranslate.translate(selectedText, languages, onGoogleTranslate);
 	}
 	else {
 		if (replaceText) {
-			googleTranslate.translate(selectedText, languages[0], onTranslated.bind(null, selection, languages[0]));
+			googleTranslate.translate(selectedText, languages[0], onGoogleTranslate.bind(null, selection, languages[0]));
 		}
 		else {
 			languages.forEach((language: string) => {
-				googleTranslate.translate(selectedText, language, onTranslated.bind(null, selection, language));
+				googleTranslate.translate(selectedText, language, onGoogleTranslate.bind(null, selection, language));
 			});
 		}
 	}
 }
 
-function onTranslated(selection: vscode.Selection, language: string, err: any, translation: any): void {
+function onGoogleTranslate(selection: vscode.Selection, language: string, err: any, translation: any): void {
 	if (err) {
 		var error: any;
 		if (err.body)
@@ -93,16 +105,14 @@ function onTranslated(selection: vscode.Selection, language: string, err: any, t
 	}
 	else if (translation.detectedSourceLanguage !== language) {
 		if (replaceText) {
-			if (selections.length === translations.length + 1) {
+			if (selections.length + linesCount === translations.length + 1) {
 				let editor = vscode.window.activeTextEditor;
 				editor.edit((editBuilder: vscode.TextEditorEdit) => {
 					for (let i = 0; i < translations.length; i++) {
 						const element = translations[i];
 						editBuilder.replace(element.selection, element.translation.translatedText);
 					}
-					editBuilder.replace(selection, translation.translatedText);					
-				}).then((value) => {
-					translations = [];
+					editBuilder.replace(selection, translation.translatedText);
 				});
 			}
 			else {
