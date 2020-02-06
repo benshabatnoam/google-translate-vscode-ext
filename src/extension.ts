ab2b6917632024f618d24a7847f6019a8ac227b8
Noam Ben Shabat <n0556771785@gmail.com>
2020-02-06 14:21:14 +0200

    traslate text by free api

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
-const translate = require("google-translate-open-api").default;
+const translateApi: (selectedText: string, configuration: {}) => Promise<any> = require("google-translate-open-api").default;
 
 var googleTranslate: any;
 var languages: any;
@@ -34,7 +34,7 @@ function onActivate(): void {
 			showEmptyError();
 			return;
 		}
-		translateSelectionFreeApi(selections[0]);
+		translateSelection(selections[0]);
 	}
 	else {
 		showEmptyError();
@@ -53,11 +53,11 @@ function initMembers(): void {
 
 function multiCursorTranslate(): void {
 	selections.forEach(selection => {
-		translateSelectionFreeApi(selection);
+		translateSelection(selection);
 	});
 }
 
-function translateSelectionPaidApi(selection: vscode.Selection | vscode.Range): void {
+function translateSelection(selection: vscode.Selection | vscode.Range): void {
 	if (!selection.isSingleLine) {
 		let firstLineNumber: number = selection.start.line;
 		let lastLineNumber = selection.end.line;
@@ -69,7 +69,7 @@ function translateSelectionPaidApi(selection: vscode.Selection | vscode.Range): 
 			} else if (lineNumber === lastLineNumber) {
 				range = new vscode.Range(lineNumber, 0, lineNumber, selection.end.character);
 			}
-			translateSelectionFreeApi(range);
+			translateSelection(range);
 		}
 		return;
 	}
@@ -79,100 +79,47 @@ function translateSelectionPaidApi(selection: vscode.Selection | vscode.Range): 
 		return;
 	}
 	if (typeof languages === "string") {
-		googleTranslate.translate(selectedText, languages, onGoogleTranslateFreeApi);
-	}
-	else {
-		if (replaceText) {
-			googleTranslate.translate(selectedText, languages[0], onGoogleTranslateFreeApi);
-		}
-		else {
-			languages.forEach((language: string) => {
-				googleTranslate.translate(selectedText, language, onGoogleTranslateFreeApi);
-			});
-		}
-	}
-}
-
-function translateSelectionFreeApi(selection: vscode.Selection | vscode.Range): void {
-	if (!selection.isSingleLine) {
-		let firstLineNumber: number = selection.start.line;
-		let lastLineNumber = selection.end.line;
-		linesCount += lastLineNumber - firstLineNumber;
-		for (let lineNumber = firstLineNumber; lineNumber <= lastLineNumber; lineNumber++) {
-			let range: vscode.Range = vscode.window.activeTextEditor.document.lineAt(lineNumber).range;
-			if (lineNumber === firstLineNumber) {
-				range = new vscode.Range(lineNumber, selection.start.character, lineNumber, range.end.character);
-			} else if (lineNumber === lastLineNumber) {
-				range = new vscode.Range(lineNumber, 0, lineNumber, selection.end.character);
-			}
-			translateSelectionFreeApi(range);
-		}
-		return;
-	}
-	let selectedText: string = vscode.window.activeTextEditor.document.getText(new vscode.Range(selection.start, selection.end));
-	if (!languages) {
-		vscode.window.showErrorMessage('Go to user settings and edit "googleTranslateExt.languages".');
-		return;
-	}
-	if (typeof languages === "string") {
-		translate(selectedText, onGoogleTranslateFreeApi);
-	}
-	else {
-		if (replaceText) {
-			translate(selectedText, onGoogleTranslateFreeApi);
-		}
-		else {
-			languages.forEach((language: string) => {
-				// translate(selectedText, onGoogleTranslate.bind(null, selection, language));
-			});
-		}
-	}
-}
-
-function onGoogleTranslateFreeApi(res: any) {
-	if (!!res && !!res.data) {
-			vscode.window.showInformationMessage(res.data[0]);
+		translate(selectedText, <vscode.Selection>selection, languages);
 	} else {
-		throw new Error("Google Translation API issue");
+		if (replaceText) {
+			translate(selectedText, <vscode.Selection>selection, languages[0]);
+		} else {
+			languages.forEach((language: string) => {
+				translate(selectedText, <vscode.Selection>selection, language);
+			});
+		}
 	}
 }
 
-function onGoogleTranslatePaidApi(selection: vscode.Selection, language: string, err: any, translation: any): void {
-	if (err) {
-		var error: any;
-		if (err.body) {
-			error += JSON.parse(err.body);
+function translate(textToTranslate: string, selection: vscode.Selection, language: string): void {
+	translateApi(textToTranslate, { to: language })
+		.then((res: any) => {
+			onTranslateSuccess(selection, language, res.data[0]);
+		})
+		.catch((error: Error) => {
+			vscode.window.showInformationMessage(error.message);
+		});
+}
+
+function onTranslateSuccess(selection: vscode.Selection, language: string, translatedText: any): void {
+	if (replaceText) {
+		if (selections.length + linesCount === translations.length + 1) {
+			let editor = vscode.window.activeTextEditor;
+			editor.edit((editBuilder: vscode.TextEditorEdit) => {
+				for (let i = 0; i < translations.length; i++) {
+					const element = translations[i];
+					editBuilder.replace(element.selection, element.translatedText);
+				}
+				editBuilder.replace(selection, translatedText);
+			});
+		} else {
+			translations.push({
+				'selection': selection,
+				'translation': translatedText
+			});
 		}
-		if (err.error) {
-			error += err.error.message;
-		}
-		if (error) {
-			console.error(error);
-		}
-		vscode.window.showErrorMessage('error ocurred on translation, see console for more details');
-	}
-	else if (translation.detectedSourceLanguage !== language) {
-		if (replaceText) {
-			if (selections.length + linesCount === translations.length + 1) {
-				let editor = vscode.window.activeTextEditor;
-				editor.edit((editBuilder: vscode.TextEditorEdit) => {
-					for (let i = 0; i < translations.length; i++) {
-						const element = translations[i];
-						editBuilder.replace(element.selection, element.translation.translatedText);
-					}
-					editBuilder.replace(selection, translation.translatedText);
-				});
-			}
-			else {
-				translations.push({
-					'selection': selection,
-					'translation': translation
-				});
-			}
-		}
-		else {
-			vscode.window.showInformationMessage(translation.translatedText);
-		}
+	} else {
+		vscode.window.showInformationMessage(translatedText);
 	}
 }
 
